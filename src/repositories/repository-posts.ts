e6eb463940388a9fa8a23df9d10c0c133eabc2ee
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2023-03-09 16:29:04 +0300

    corrected post method

--- src/repositories/repository-posts.ts
+++ src/repositories/repository-posts.ts
@@ -2,7 +2,7 @@ import {PostType} from "../types";
 import {blogsControl} from "./repository-blogs";
 
 
-export let arrPosts: PostType[] = [ {
+export let arrPosts: PostType[] = [{
     id: "1",
     title: "Super title",
     shortDescription: "bad men",
@@ -28,33 +28,40 @@ export let arrPosts: PostType[] = [ {
     }]
 
 export const postsControl = {
-    getAllPosts(){
+    getAllPosts() {
         return arrPosts
     },
-    getPostById(id:string){
+    getPostById(id: string) {
         return arrPosts.find((it) => it.id.toString() === id)
     },
-    createPost(body: PostType){
+    createPost(body: PostType) {
         const findBlog = blogsControl.getBlogById(body.blogId)
-        if(findBlog){
+        if (findBlog) {
             const newPost = {
                 id: (+(new Date())).toString(),
                 title: body.title,
-                shortDescription:body.shortDescription,
+                shortDescription: body.shortDescription,
                 content: body.content,
                 blogId: body.blogId,
                 blogName: findBlog.name
             }
             arrPosts.push(newPost)
-            return newPost.id
+            return {
+                "blogId": newPost.blogId,
+                "blogName": newPost.blogName,
+                "content": newPost.content,
+                "id": newPost.id,
+                "shortDescription": newPost.shortDescription,
+                "title": newPost.title
+            }
         } else {
             return null
         }
     },
-    changePost(id: string, body:PostType){
+    changePost(id: string, body: PostType) {
         const findPost = postsControl.getPostById(id)
         const findBlog = blogsControl.getBlogById(body.blogId)
-        if(findPost && findBlog){
+        if (findPost && findBlog) {
             findPost.blogId = body.blogId
             findPost.content = body.content
             findPost.title = body.title
@@ -62,21 +69,21 @@ export const postsControl = {
             findPost.blogName = findBlog.name
 
             return true
-        } else{
-            return  false
+        } else {
+            return false
         }
 
     },
-    deletePost(id:string){
+    deletePost(id: string) {
         const findPost = postsControl.getPostById(id)
-        if(findPost){
-            arrPosts = arrPosts.filter(el=> el.id !== id)
-            return  true
+        if (findPost) {
+            arrPosts = arrPosts.filter(el => el.id !== id)
+            return true
         }
         return false
     },
-    deleteAllPosts(){
-        arrPosts= []
+    deleteAllPosts() {
+        arrPosts = []
     }
 
 }
